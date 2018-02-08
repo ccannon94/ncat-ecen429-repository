5e60d91ba456db5de2f9a9672c73789ce04e81ce
Chris Cannon <chris.c.cannon@gmail.com>
2018-02-08 14:39:28 -0500

    More Part 2

--- Lab2/Lab2Part3/Lab2Part3.runs/impl_1/Lab2Part3.tcl
+++ Lab2/Lab2Part3/Lab2Part3.runs/impl_1/Lab2Part3.tcl
@@ -65,7 +65,6 @@ start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  set_param xicom.use_bs_reader 1
   open_checkpoint Lab2Part3_routed.dcp
   set_property webtalk.parent_dir C:/Users/student/Desktop/ncat-ecen429-repository/Lab2/Lab2Part3/Lab2Part3.cache/wt [current_project]
   catch { write_mem_info -force Lab2Part3.mmi }
