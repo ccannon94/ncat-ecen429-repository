a157c56fa5f95b1e4ceda7d59ebe356bde0e713b
Chris Cannon <chris.c.cannon@gmail.com>
2018-02-08 14:01:23 -0500

    Lab 3 progress

---
+++ Lab3/Lab3Part2/Lab3Part2.srcs/sources_1/new/Lab3Part2.vhd
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/08/2018 02:01:01 PM
+-- Design Name: 
+-- Module Name: Lab3Part2 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Lab3Part2 is
+    Port ( cin : in STD_LOGIC;
+           a1 : in STD_LOGIC;
+           a2 : in STD_LOGIC;
+           b1 : in STD_LOGIC;
+           b2 : in STD_LOGIC;
+           x1 : out STD_LOGIC;
+           x2 : out STD_LOGIC;
+           cout : out STD_LOGIC;
+           sel : in STD_LOGIC);
+end Lab3Part2;
+
+architecture Behavioral of Lab3Part2 is
+
+begin
+
+
+end Behavioral;
