cc5a98eec83cc1618fc1fbf809f930f0e6f18936
Chris Cannon <chris.c.cannon@gmail.com>
2018-03-01 14:49:30 -0500

    Start Part2

--- Lab6/Lab6Part2/Lab6Part2.srcs/sources_1/new/moore_machine.vhd
+++ Lab6/Lab6Part2/Lab6Part2.srcs/sources_1/new/moore_machine.vhd
@@ -4,15 +4,26 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity moore_machine is
     Port ( input : in STD_LOGIC;
+           clk : in STD_LOGIC;
+           enable : in STD_LOGIC;
            led0 : out STD_LOGIC;
-           state : out STD_LOGIC;
-           output : out STD_LOGIC;
-           clk : in STD_LOGIC);
+           state : out STD_LOGIC_VECTOR(9 downto 0);
+           output : out STD_LOGIC_VECTOR(6 downto 0));
 end moore_machine;
 
 architecture moore_arch of moore_machine is
+component Clockdivider is
+    Port ( clk : in STD_LOGIC;
+           start_timer : in STD_LOGIC;
+           FastClock,MediumClock,SlowClock,led0 : out STD_LOGIC);
+end component Clockdivider;
+
+signal slowClock : STD_LOGIC;
+signal mediumClock : STD_LOGIC;
+signal fastClock : STD_LOGIC;
 
 begin
-
+    clock : Clockdivider port map(clk, enable, slowClock, mediumClock, fastClock, led0);
+    
 
 end moore_arch;
