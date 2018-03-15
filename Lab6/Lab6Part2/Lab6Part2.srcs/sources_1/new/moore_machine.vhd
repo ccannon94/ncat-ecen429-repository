b543cd8420115f9b0aac1336bbe0b11fff2fa8e0
Chris Cannon <chris.c.cannon@gmail.com>
2018-03-15 12:39:36 -0400

    did some stuff

--- Lab6/Lab6Part2/Lab6Part2.srcs/sources_1/new/moore_machine.vhd
+++ Lab6/Lab6Part2/Lab6Part2.srcs/sources_1/new/moore_machine.vhd
@@ -21,9 +21,26 @@ end component Clockdivider;
 signal slowClock : STD_LOGIC;
 signal mediumClock : STD_LOGIC;
 signal fastClock : STD_LOGIC;
+signal current_state : STD_LOGIC_VECTOR(3 downto 0) := "0000";
 
 begin
-    clock : Clockdivider port map(clk, enable, slowClock, mediumClock, fastClock, led0);
     
+    clock : Clockdivider port map(clk, enable, slowClock, mediumClock, fastClock, led0);
+    process(clk, enable)
+    begin
+        if(clk'event and (clk = '1') and (enable = '1')) then
+            if(current_state = "1001") then
+                current_state <= "0000";
+            else
+                current_state <= std_logic_vector( unsigned(current_state) + 1 );
+            end if;
+            if((current_state = "0100") or (current_state = "1000")) then
+                output <= "1001111";
+            else
+                output <= "1111111";
+            end if;
+        end if;
+    end process;    
+            
 
 end moore_arch;
