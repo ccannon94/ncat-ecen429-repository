46ccd5eb0102d6789e3d455d9facef934a4a0773
Chris Cannon <chris.c.cannon@gmail.com>
2018-04-10 15:50:09 -0400

    Add the basics of lab9part2 to lab9part3

---
+++ Lab9/Lab9Part3/Lab9Part3.srcs/sources_1/new/traffic_count.vhd
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity traffic_count is
+    port(input : in STD_LOGIC);
+end entity traffic_count;
+
+architecture Behavioral of traffic_count is
+begin
+end Behavioral;
